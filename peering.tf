3de954a27703aaf67e4499ea4306f87230651014
Sundhar Ajmera <aitechwithsundhar@gmail.com>
2026-05-23 12:47:43 +0700

    action

---
+++ peering.tf
@@ -0,0 +1,21 @@
+resource "aws_vpc_peering_connection" "peer" {
+    count = var.create_peering ? 1 : 0
+    peer_vpc_id   = data.aws_vpc.default.id # accepter VPC ID
+    vpc_id        = aws_vpc.main.id # requester VPC I
+    auto_accept   = true 
+    accepter {
+        allow_remote_vpc_dns_resolution = true
+    }
+    requester {
+        allow_remote_vpc_dns_resolution = true
+    }
+
+
+  tags = merge(
+    local.common_tags,
+    {
+      Name = "${var.project}-${var.environment}-default"
+    },
+    var.peering_tags
+  )
+}
