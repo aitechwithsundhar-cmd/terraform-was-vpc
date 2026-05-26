f50464015ab371365d762bdfb7e67adbeed8c224
Sundhar Ajmera <aitechwithsundhar@gmail.com>
2026-05-26 13:24:23 +0700

    Fixed default VPC peering route

--- peering.tf
+++ peering.tf
@@ -46,9 +46,10 @@ resource "aws_route" "database_peering" {
   vpc_peering_connection_id = aws_vpc_peering_connection.peer[count.index].id
 }
 
-# resource "aws_route" "default_peering" {
-#   count                     = var.is_peering_required ? 1 : 0
-#   route_table_id            = data.aws_route_table.default.id
-#   destination_cidr_block    = aws_vpc.main.cidr_block
-#   vpc_peering_connection_id = aws_vpc_peering_connection.peer[count.index].id
-# }
+resource "aws_route" "default_peering" {
+  count = var.is_peering_required ? 1 : 0
+
+  route_table_id            = data.aws_route_table.default.id
+  destination_cidr_block    = aws_vpc.main.cidr_block
+  vpc_peering_connection_id = aws_vpc_peering_connection.peer[count.index].id
+}
